6c0477f496ab7125ad1f319de6543ec008f635cc
erupter <erupter@tiscali.it>
2013-01-08 16:10:18 +0100

    moved some defines

---
+++ srcService/globals.h
@@ -0,0 +1,6 @@
+#define THREAD_CONTROL_IPC      "ipc://saetta/th_control"
+#define MULTICAST_ADDRESS       "239.192.1.1"
+#define MULTICAST_PORT          5678
+#define ROUTER_PORT             5679
+#define HEADER_CTRL             "CTRL"
+#define HEADER_SERVER_INFO      "SERVER_INFO"
